10ea656182a96d05fe9ca29ffb17ad7d2c0e819f
q <q@q>
2019-11-17 22:18:12 +0300

    feat: add usage example

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install ansible -y
+ansible-galaxy install -r requirements.yml
+ansible-playbook -K -i hosts.yml install-packages.yml
+ansible-playbook -K -i hosts.yml install-docker.yml
