f81c8a97e8afbc54d8f8a25a25f17747ec403291
q <q@q>
2019-11-18 11:06:53 +0300

    feat: copy configs from files

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 sudo apt install ansible -y
 ansible-galaxy install -r requirements.yml
 ansible-playbook -K -i hosts.yml install-packages.yml
+ansible-playbook -K -i hosts.yml copy-configs.yml
 ansible-playbook -K -i hosts.yml install-docker.yml
